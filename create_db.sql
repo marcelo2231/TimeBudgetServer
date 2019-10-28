01f1e99e228b1dc229316a42cba6cfade424fd12
Joseph Szendre <joseph.szendre@techcyte.com>
2019-10-28 14:00:25 -0600

    Add empty database and creation script

---
+++ create_db.sql
@@ -0,0 +1,45 @@
+CREATE TABLE users
+(id INTEGER NOT NULL,
+name VARCHAR(50) NOT NULL,
+email VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL,
+created_at INTEGER,
+PRIMARY KEY (id))
+
+CREATE TABLE auth
+(id INTEGER NOT NULL,
+user_id INTEGER NOT NULL,
+expires_at INTEGER NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY(user_id) REFERENCES users(id))
+
+CREATE TABLE time_periods
+(id INTEGER NOT NULL,
+user_id INTEGER NOT NULL,
+start_at INTEGER NOT NULL,
+end_at INTEGER NOT NULL,
+deleted_at INTEGER,
+PRIMARY KEY (id),
+FOREIGN KEY(user_id) REFERENCES users(id))
+
+
+CREATE TABLE categories
+(id INTEGER NOT NULL,
+description VARCHAR(100),
+user_id INTEGER NOT NULL,
+deleted_at INTEGER,
+PRIMARY KEY (id),
+FOREIGN KEY(user_id) REFERENCES users(id))
+
+CREATE TABLE events
+(id INTEGER NOT NULL,
+category_id INTEGER NOT NULL,
+description VARCHAR(100),
+start_at INTEGER NOT NULL,
+end_at INTEGER NOT NULL,
+user_id INTEGER NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY(user_id) REFERENCES users(id),
+FOREIGN KEY(category_id) REFERENCES categories(id)
+)
+
