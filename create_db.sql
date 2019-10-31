a9487f07452779ed6b90d89a095cff729ac44326
Joseph Szendre <joseph.szendre@techcyte.com>
2019-10-31 17:40:29 -0600

    sql changes

--- create_db.sql
+++ create_db.sql
@@ -4,14 +4,14 @@ username VARCHAR(50) NOT NULL,
 email VARCHAR(100) NOT NULL,
 password VARCHAR(100) NOT NULL,
 created_at INTEGER,
-PRIMARY KEY (id))
+PRIMARY KEY (id));
 
 CREATE TABLE auth
 (id INTEGER NOT NULL,
 user_id INTEGER NOT NULL,
 expires_at INTEGER NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY(user_id) REFERENCES users(id))
+FOREIGN KEY(user_id) REFERENCES users(id));
 
 CREATE TABLE time_periods
 (id INTEGER NOT NULL,
@@ -20,7 +20,7 @@ start_at INTEGER NOT NULL,
 end_at INTEGER NOT NULL,
 deleted_at INTEGER,
 PRIMARY KEY (id),
-FOREIGN KEY(user_id) REFERENCES users(id))
+FOREIGN KEY(user_id) REFERENCES users(id));
 
 
 CREATE TABLE categories
@@ -29,7 +29,7 @@ description VARCHAR(100),
 user_id INTEGER NOT NULL,
 deleted_at INTEGER,
 PRIMARY KEY (id),
-FOREIGN KEY(user_id) REFERENCES users(id))
+FOREIGN KEY(user_id) REFERENCES users(id));
 
 CREATE TABLE events
 (id INTEGER NOT NULL,
@@ -40,6 +40,4 @@ end_at INTEGER NOT NULL,
 user_id INTEGER NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY(user_id) REFERENCES users(id),
-FOREIGN KEY(category_id) REFERENCES categories(id)
-)
-
+FOREIGN KEY(category_id) REFERENCES categories(id));
