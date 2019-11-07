b800b27117c58e8684ae36dac610d79bb909f3bb
Joseph Szendre <joseph.szendre@techcyte.com>
2019-11-06 21:10:43 -0700

    have fake_it properly look up the category ids

--- src/main/java/timebudget/handlers/FakeItHandler.java
+++ src/main/java/timebudget/handlers/FakeItHandler.java
@@ -2,13 +2,17 @@ package timebudget.handlers;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 
 import com.sun.net.httpserver.HttpExchange;
 
 import timebudget.ServerFacade;
+import timebudget.exceptions.UserCreationException;
 import timebudget.handlers.HandlerBase;
 import timebudget.log.Corn;
+import timebudget.model.Category;
 import timebudget.model.Event;
 import timebudget.model.User;
 
@@ -29,17 +33,42 @@ public class FakeItHandler extends HandlerBase {
 		Boolean pass = true;
 
 		User u = new User(User.NO_USER_ID, "test_user", "test@gmail.com","password", User.NO_CREATED_AT);
-		u = ServerFacade.getInstance().register(u);
+		try {
+			u = ServerFacade.getInstance().register(u);
+		} catch (UserCreationException e) {
+			Random r = new Random();
+			u.setUsername("test_user" + String.valueOf(r.nextInt(10000)));
+			u = ServerFacade.getInstance().register(u);
+		}
+		
+		int schoolCategoryID = -1;
+		int amusementCategoryID = -1;
+		int healthCategoryID = -1;
+		List<Category> categories = ServerFacade.getInstance().getAllActiveCategories(u);
+
+		for (Category c: categories) {
+			if (c.getDescription().equals("Amusement"))
+				amusementCategoryID = c.getCategoryID();
+			
+			if (c.getDescription().equals("School"))
+				amusementCategoryID = c.getCategoryID();
+
+			if (c.getDescription().equals("Health/Wellness"))
+				healthCategoryID = c.getCategoryID();
+		}
 
 		// books events
-		Event ev1 = new Event(Event.NO_EVENT_ID, 3, "Reading War and Peace", u.getUserID(), 3600, 3600 * 3);
-		Event ev2 = new Event(Event.NO_EVENT_ID, 3, "Reading Anna Karenina", u.getUserID(), 3600 * 3, 3600 * 9);
+		Event ev1 = new Event(Event.NO_EVENT_ID, schoolCategoryID, "Reading War and Peace", u.getUserID(), 3600, 3600 * 3);
+		Event ev2 = new Event(Event.NO_EVENT_ID, schoolCategoryID, "Reading Anna Karenina", u.getUserID(), 3600 * 3, 3600 * 9);
 
 		// amusement events
-		Event ev3 = new Event(Event.NO_EVENT_ID, 6, "Watching The Office", u.getUserID(), 3600 * 10, 3600 * 11);
-		Event ev4 = new Event(Event.NO_EVENT_ID, 6, "Watching Stranger Thangs", u.getUserID(), 3600 * 12, 3600 * 15);
-		
-		Event[] events = {ev1, ev2, ev3, ev4};
+		Event ev3 = new Event(Event.NO_EVENT_ID, amusementCategoryID, "Watching The Office", u.getUserID(), 3600 * 10, 3600 * 11);
+		Event ev4 = new Event(Event.NO_EVENT_ID, amusementCategoryID, "Watching Stranger Thangs", u.getUserID(), 3600 * 12, 3600 * 15);
+
+		// health events
+		Event ev5 = new Event(Event.NO_EVENT_ID, healthCategoryID, "Work out", u.getUserID(), 3600 * 15, 3600 * 17);
+
+		Event[] events = {ev1, ev2, ev3, ev4, ev5};
 		for (Event e : events)
 			ServerFacade.getInstance().createEvent(u, e);
 
